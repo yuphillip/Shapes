6f702af2fc422a9236c7e0bcdc62bf93243f4405
BT_1N3_09 <mmoore4262@bths.edu>
2018-10-01 10:10:16 -0400

    Shape with Runner

--- src/Shape.java
+++ src/Shape.java
@@ -11,12 +11,41 @@ public abstract class Shape {
         return "I am a" + type + ".";
     }
 }
-/*
+
 public class Runner
 {
     public static void main (String[]args)
     {
-        Shape shape1 = new  Rectangle()
+        Shape shape1 = new  Rectangle(height:10, width:5);
+        Shape shape2 = new Etriangle(side:5);
+        Shape shape3 = new Circle(radius:10);
+        
+        Shape shape1 = new RPentagon(side:5);
+        System.out.println(shape4);
+        System.out.println("My Perimeter is: "+shape4.getPerimeter());
+        System.out.println("My area is: "+ shape4.getArea());
+
+        Shape[] shapes={shape1, shape2, shape3};
+
+        for (Shape cur:shapes)
+        {
+            System.out.println(cur);
+            System.out.println("My area is: "+cur.getArea());
+            System.out.println("My perimeter is: "+ cur.getPerimeter());
+        }
     }
 }
-*/
+
+
+   public class Rectangle extends Shape
+   {
+       //Fields
+       int height;
+       int width;
+       Constructor
+       public Rectangle(int height, int width) //Don’t forget to initialize type
+       Methods
+       getArea()
+       getPerimeter()
+   }
+
