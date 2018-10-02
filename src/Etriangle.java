628a8d3468a89bdbd2856b3fd108be458a386e3b
BT_1N3_09 <mmoore4262>
2018-10-02 09:40:52 -0400

    Etriangle

--- src/Etriangle.java
+++ src/Etriangle.java
@@ -1,2 +1,19 @@
-public class Etriangle {
+/*Matthew Moore*/
+public class Etriangle extends Shape {
+    private int side;
+
+    public Etriangle(int side)
+    {
+        super(type:"Etriangle");
+        this.side = side;
+    }
+    public double getArea()
+    {
+        return ((1.732/4)*(side*side));
+    }
+
+    public double getPerimeter()
+    {
+        return (side*3);
+    }
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,21 +1,3 @@
 /*Yu Phillip
 */
  */
-public class Rectangle extends Shape {
-    private int side;
-
-    public Rectangle(int side)
-    {
-        super("Rectangle");
-        this.side = side;
-    }
-    public double getArea()
-    {
-        return (height*width);
-    }
-
-    public double getPerimeter()
-    {
-        return side*4;
-    }
-}
